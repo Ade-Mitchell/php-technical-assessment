8150d1b6148ae734b76333f30b7125433060cb7c
Ade-Mitchell <ade@mitchell.cymru>
2026-04-02 23:15:15 +0100

    Add shared layout to edit page

--- resources/views/books/edit.php
+++ resources/views/books/edit.php
@@ -1,40 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Book</title>
-</head>
-<body>
-<h1>Edit Book</h1>
+<?php ob_start(); ?>
 
-<p><a href="/books">Back to books</a></p>
+    <h1>Edit Book</h1>
 
-<form action="/books/update" method="POST">
-    <input type="hidden" name="id" value="<?= htmlspecialchars((string) $book['id']) ?>">
+    <p><a href="/books">Back to books</a></p>
 
-    <div>
-        <label>Title</label><br>
-        <input type="text" name="title" value="<?= htmlspecialchars($book['title']) ?>">
-    </div>
+    <form action="/books/update" method="POST">
+        <input type="hidden" name="id" value="<?= htmlspecialchars((string) $book['id']) ?>">
 
-    <br>
+        <div>
+            <label>Title</label><br>
+            <input type="text" name="title" value="<?= htmlspecialchars($book['title']) ?>">
+        </div>
 
-    <div>
-        <label>Author</label><br>
-        <input type="text" name="author" value="<?= htmlspecialchars($book['author']) ?>">
-    </div>
+        <br>
 
-    <br>
+        <div>
+            <label>Author</label><br>
+            <input type="text" name="author" value="<?= htmlspecialchars($book['author']) ?>">
+        </div>
 
-    <div>
-        <label>Published Year</label><br>
-        <input type="number" name="published_year" value="<?= htmlspecialchars((string) $book['published_year']) ?>">
-    </div>
+        <br>
 
-    <br>
+        <div>
+            <label>Published Year</label><br>
+            <input type="number" name="published_year" value="<?= htmlspecialchars((string) $book['published_year']) ?>">
+        </div>
 
-    <button type="submit">Update</button>
-</form>
-</body>
-</html>
+        <br>
+
+        <button type="submit">Update</button>
+    </form>
+
+<?php
+$content = ob_get_clean();
+$title = 'Edit Book';
+require __DIR__ . '/../layout.php';
--- resources/views/books/index.php
+++ resources/views/books/index.php
@@ -1,28 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Books</title>
-</head>
-<body>
-<h1>Books</h1>
+<?php ob_start(); ?>
 
-<?php if (empty($books)): ?>
-    <p>No books found.</p>
-<?php else: ?>
+    <h1>Books</h1>
+
+    <p><a href="/books/create">Add Book</a></p>
+
     <ul>
         <?php foreach ($books as $book): ?>
             <li>
                 <?= htmlspecialchars($book['title']) ?>
-                by
-                <?= htmlspecialchars($book['author']) ?>
-                (<?= htmlspecialchars((string) $book['published_year']) ?>)
                 -
                 <a href="/books/edit?id=<?= $book['id'] ?>">Edit</a>
             </li>
         <?php endforeach; ?>
     </ul>
-<?php endif; ?>
-</body>
-</html>
+
+<?php
+$content = ob_get_clean();
+$title = 'Books';
+require __DIR__ . '/../layout.php';
