b177a63285ceab4a2018d95b46cb81553aa9184c
Ade-Mitchell <ade@mitchell.cymru>
2026-04-02 23:02:09 +0100

    Add find method to Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -13,4 +13,14 @@ class Book
         $stmt = Database::connection()->query('SELECT * FROM books ORDER BY id DESC');
         return $stmt->fetchAll();
     }
+
+    public function find(int $id): ?array
+    {
+        $stmt = Database::connection()->prepare('SELECT * FROM books WHERE id = :id');
+        $stmt->execute(['id' => $id]);
+
+        $book = $stmt->fetch();
+
+        return $book ?: null;
+    }
 }
